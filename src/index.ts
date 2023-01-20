0d1014d5e296418f9e6d70a19c86bc916e574632
Gabriel Valera <gabrielj2814@hotmail.com>
2023-01-20 14:26:13 -0400

    add agregando configuracion de ts-node para debuf con typescript

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let nombre:string = "gabriel valera"
+let edad:number=17
+
+if(edad>=18){
+    console.log("es mayor de edad")
+}
+else{
+    console.log("es menor de edad")
+}
+
+console.log("nombre de usuario => ",nombre)
